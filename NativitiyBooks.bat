33497eb69ea245f6ce9d5e8958ad4d3cf9d8f36f
rfern2000 <61155401+rfern2000@users.noreply.github.com>
2020-02-18 11:51:26 -0500

    Create NativitiyBooks.bat

---
+++ NativitiyBooks.bat
@@ -0,0 +1,55 @@
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/book.git
+cd book
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/inventory.git
+cd inventory
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/dBAccess.git
+cd dBAccess
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/prints.git
+cd prints
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/records.git
+cd records
+git checkout develop
+cd .. 
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/multimedia.git
+cd multimedia
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/controlRegister.git
+cd controlRegister
+git checkout develop
+cd .. 
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/controlRegisterOrchestrator.git
+cd controlRegisterOrchestrator
+git checkout develop
+cd ..
+copy book/DockerfileNativitiyBooks Dockerfile
+
+###########################################################################
+
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/person.git
+cd person
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/sheets.git
+cd sheets
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/nativityInscription.git
+cd nativityInscription
+git checkout develop
+cd ..
+git clone https://alexander.q.quintero%40oracle.com@rnlpm99dev-aucrnph01.developer.ocp.oraclecloud.com/rnlpm99dev-aucrnph01/s/rnlpm99dev-aucrnph01_rnp_2703/scm/nativityInscriptionOrchestrator.git
+cd nativityInscriptionOrchestrator
+git checkout develop
+cd ..
+
+copy book/DockerfileNativitiyRecords Dockerfile
+##############################################################################
